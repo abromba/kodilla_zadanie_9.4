3f81fb724aef2e5ccf547f8330db7fb27571022b
Ania <Ania>
2017-05-07 19:52:52 +0200

    simetric tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
-function drawTree (stages) {
-    for ( var i = 0 ; i < stages ; i++) {
-        var star = ' ';
-    for (var j = 0; j < i; j++) {
-        star += '*';	
+//CHOINKA SYMETRYCZNA - trochę wspierałam sie ksiażką
+function drawTree(stages) {
+    for ( var i = 1; i < stages; i++) {
+        var empty = ' ';
+		var star = '*';
+        for (var j = 1; j < stages-i; j++) {
+            empty += ' ';	
+        }
+		for (var j = 1; j < i*2-1; j++) {
+            star += '*';
+        }
+    console.log(empty, star);
     }
-    console.log(star);
 }
+stages = prompt('Podaj ilość pięter');
+drawTree(stages);
+
+/* MOJA CHOINKA NIESYMETRYCZNA
+
+function drawTree(stages) {
+    for (var i = 0 ; i < stages; i++) {
+        var line = '';
+        for (var j = 0; j < i; j++) {
+            line += '*';	
+        }
+    console.log(line);
+    }
 }
-stages = prompt ('Podaj ilość pięter');
-drawTree (stages);
+stages = prompt('Podaj ilość pięter');
+drawTree(stages);
+*/
