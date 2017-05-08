40bf6e488e2c69c3f5458031d447a2347b60f5bd
Ania <Ania>
2017-05-08 09:01:13 +0200

    modifications of symetric tree, comments removed

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,14 @@
-//CHOINKA SYMETRYCZNA - trochę wspierałam sie ksiażką
 function drawTree(stages) {
-    for ( var i = 1; i < stages; i++) {
-        var empty = ' ';
-		var star = '*';
-        for (var j = 1; j < stages-i; j++) {
-            empty += ' ';	
-        }
-		for (var j = 1; j < i*2-1; j++) {
-            star += '*';
-        }
-    console.log(empty, star);
-    }
-}
-stages = prompt('Podaj ilość pięter');
-drawTree(stages);
-
-/* MOJA CHOINKA NIESYMETRYCZNA
-
-function drawTree(stages) {
-    for (var i = 0 ; i < stages; i++) {
+    for (var i = 0; i < stages; i++) {
         var line = '';
-        for (var j = 0; j < i; j++) {
-            line += '*';	
+        for (var j = 0; j < stages-i; j++) {
+            line += ' ';
+        }
+        for (var k = 0; k < i * 2 + 1; k++) {
+            line += '*';
         }
     console.log(line);
     }
 }
-stages = prompt('Podaj ilość pięter');
+var stages = prompt('Podaj ilość pięter');
 drawTree(stages);
-*/
